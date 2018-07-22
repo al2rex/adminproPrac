51031e52c09574f46ba53956581526280b8f08af
Willian paternina <ingwillianpaternina2101@gmail.com>
2018-07-22 10:11:52 -0500

    Agregamos y modificamos modulos

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+
+import { SharedModule } from '../shared/shared.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { PagesComponent } from './pages.component';
+import { PAGES_ROUTE } from './pages.route';
+
+
+@NgModule({
+    declarations: [
+        PagesComponent,
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component,
+    ],
+    exports: [
+        PagesComponent,
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component,
+    ],
+    imports: [
+        SharedModule,
+        PAGES_ROUTE,
+    ]
+})
+
+export class PagesModule{ }
